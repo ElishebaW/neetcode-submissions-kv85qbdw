efb9378ba161f709503aa5eb342546924e14cb7e
Elisheba Anderson <leighw33@gmail.com>
2026-04-06 11:02:26 -0400

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """
+        1- obstacle
+        0 - space
+        [[0,0,0],
+        [0,0,0],
+        [0,1,0]]
+        """
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0] * n
+        dp[n - 1] = 1
+
+        for r in reversed(range(m)):
+            for c in reversed(range(n)):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < n:
+                    dp[c] +=  dp[c + 1]
+        
+        return dp[0]
+
+
+        
