08d35792efd2e9417a2dc866d79b7b486aa5d5e4
Elisheba Anderson <leighw33@gmail.com>
2026-04-20 09:32:50 -0400

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # T: O(n log m)
+        # S: O(1)
+
+        #left = 1
+        #right = max of any pile
+        #loop
+        # calculates the mid
+        # total hours for the mid
+        # total hours is less than hours  move left setting right to mi
+        # if total hours is more than move right set left to mid +1 
+        # return right which will be the min to eat all bananas
+
+        left = 1
+        right = max(piles)
+
+        while left < right:
+            mid = (left + right)//2
+
+            total_hours = sum(( p + mid - 1)//mid for p in piles)
+
+            if total_hours <= h:
+                right = mid
+            else:
+                left = mid + 1
+        
+
+        return right
